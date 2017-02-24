4a1c7f2f3a0070e4b6813b4a5a4fa53955691ee1
chrisbacon <bacon.chris@gmail.com>
2017-02-24 15:59:08 +0000

    initial commit

---
+++ desktop_client/client/src/app.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+window.onload = function () {
+  ReactDOM.render(
+    <h1>App Started</h1>,
+    document.getElementById('app')
+  );
+}
