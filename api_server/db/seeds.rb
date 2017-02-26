e170d112332b3292df1b1d76e2199345dc9685c5
Will Robertson <w.robertson305@gmail.com>
2017-02-26 13:07:11 +0000

    set up db hierarchy for course

--- api_server/db/seeds.rb
+++ api_server/db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Course.destroy_all
+Lesson.destroy_all
+Exercise.destroy_all
+
+c1 = Course.create(
+{
+  name: 'test course'
+}
+)
+
+l1 = Lesson.create(
+{
+  name: 'test lesson',
+  course: c1,
+  position: 1
+}
+)
+
+e1 = Exercise.create(
+{
+  name: 'test exercise',
+  lesson: l1,
+  position: 1
+}
+)
