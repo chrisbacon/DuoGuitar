daba5263948ebfe780cd2f58b921b124101a645b
William Robertson <w.robertson305@gmail.com>
2017-02-27 11:57:40 +0000

    add enrolled flag for courses

--- desktop_client/client/src/containers/duoguitar.jsx
+++ desktop_client/client/src/containers/duoguitar.jsx
@@ -9,30 +9,42 @@ class DuoGuitar extends React.Component {
     this.requester = new Requester()
 
     this.state = {
-      user: props.user,
       courses: [],
       selectedCourse: null
     }
 
     this.selectCourse = this.selectCourse.bind(this);
-    this.handleRequest = this.handleRequest.bind(this);
+    this.populateCourses = this.populateCourses.bind(this);
   }
 
   componentDidMount(){
-    console.log(this.state.user)
-    this.requester.makeRequest({codeDesired: 200, url: 'http://localhost:5000/api/courses', type: 'GET', body: '', callback: this.handleRequest})
+    this.requester.makeRequest({codeDesired: 200, url: 'http://localhost:5000/api/courses', type: 'GET', body: '', callback: this.populateCourses})
   }
 
-  handleRequest(responseObject){
+  populateCourses(responseObject){
     if (!responseObject.error){
       this.setState( { courses: responseObject.response } )
     }
+    this.filterCoursesByEnrolled()
+  }
+
+  filterCoursesByEnrolled(){
+    var userCourses = this.props.user.courses;
+    var courses = this.state.courses;
+    for(var i=courses.length-1; i>=0; i--) {
+      for (var y=0; y<(userCourses.length); y++){
+        if(courses[i].id === userCourses[y].id){
+          courses[i].enrolled = true;
+        }
+      }
+    }
+    this.setState(courses: courses)
   }
 
   selectCourse(course) {
     this.setState({selectedCourse: course})
   }
-
+  
   render () {
     console.log('selectedCourse ', this.state.selectedCourse)
     if (this.state.selectedCourse) {
