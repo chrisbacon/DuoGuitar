ea7028d2072d286877006100ee4cbe28b54728dc
chrisbacon <bacon.chris@gmail.com>
2017-02-27 15:06:31 +0000

    refactored so login box only handles sign in and sign up

--- desktop_client/client/src/auth/LoginBox.jsx
+++ desktop_client/client/src/auth/LoginBox.jsx
@@ -1,66 +1,22 @@
 import React from 'react'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
-import SignOut from './SignOut'
-import Requester from '../components/requester'
-import DuoGuitar from '../containers/duoguitar'
+
 
 class LoginBox extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentUser: null
-    }
-    this.setUser = this.setUser.bind(this);
-    this.requester = new Requester();
-    this.userFetched = this.userFetched.bind(this);
-  }
+  
 
-  setUser(user){
-    console.log("user: ", user)
-    this.setState({currentUser:user, courselist:[]});
-  }
-
-  fetchUser(){
-    console.log("fetching user");
-    this.requester.makeRequest({codeDesired: 200, url: 'http://localhost:5000/users.json', type: 'GET', body: '', callback: this.userFetched})
-  }
-
-  userFetched(responseObject){
-    if (!responseObject.error){
-      var receivedUser = responseObject.response;
-      this.setUser(receivedUser);
-    } else {
-      this.setUser(null);
-    }
-  }
-
-  componentDidMount(){
-    this.fetchUser()
-  }
+  
 
   render () {
-    var mainDiv = <div>
+    return (<div>
       <h4> Please Sign In/Up </h4>
-      <SignIn url={this.props.url + "users/sign_in.json"} onSignIn={this.setUser}></SignIn>
-      <SignUp url={this.props.url + "users.json"} onSignUp={this.setUser}></SignUp>
-    </div>
+      <SignIn url={this.props.url + "users/sign_in.json"} onSignIn={this.props.setUser}></SignIn>
+      <SignUp url={this.props.url + "users.json"} onSignUp={this.props.setUser}></SignUp>
+    </div>)
 
-    if(this.state.currentUser){
-
-      mainDiv = <div>
-        <h4> Welcome {this.state.currentUser.email}</h4>
-        <SignOut url={this.props.url + "users/sign_out.json"} onSignOut={this.setUser}></SignOut>
-        <DuoGuitar user={this.state.currentUser}></DuoGuitar>
-      </div>
-
-    }
-    return (
-      <div>
-        { mainDiv }
-      </div>
-    )
+    
   }
 }
 
--- desktop_client/client/src/containers/Home.jsx
+++ desktop_client/client/src/containers/Home.jsx
@@ -1,19 +1,60 @@
 import React from 'react'
 import LoginBox from '../auth/LoginBox'
+import SignOut from '../auth/SignOut'
+import Requester from '../components/requester'
+import DuoGuitar from '../containers/duoguitar'
 
 class Home extends React.Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      currentUser: null
   }
+  this.setUser = this.setUser.bind(this);
+  this.requester = new Requester();
+  this.userFetched = this.userFetched.bind(this);
+}
 
-  render() {
+setUser(user){
+    console.log("user: ", user)
+    this.setState({currentUser:user});
+}
 
-    return (
-      <LoginBox url="http://localhost:5000" />
-      )
+fetchUser(){
+    console.log("fetching user");
+    this.requester.makeRequest({codeDesired: 200, url: 'http://localhost:5000/users.json', type: 'GET', body: '', callback: this.userFetched})
+}
 
+userFetched(responseObject){
+    if (!responseObject.error){
+      var receivedUser = responseObject.response;
+      this.setUser(receivedUser);
+  } else {
+      this.setUser(null);
   }
+}
+
+componentDidMount(){
+    this.fetchUser()
+}
+
+render() {
+    if(this.state.currentUser){
+        return(
+            <div>
+                <nav>
+                    <h4> Welcome {this.state.currentUser.email}</h4>
+                    <SignOut url={this.props.url + "users/sign_out.json"} onSignOut={this.setUser}></SignOut>
+                </nav>
+                <DuoGuitar user={this.state.currentUser}/>
+            </div>
+        )
+
+    }
+    return <LoginBox setUser={this.setUser} url="http://localhost:5000/" />
+
+}
 
 }
 
