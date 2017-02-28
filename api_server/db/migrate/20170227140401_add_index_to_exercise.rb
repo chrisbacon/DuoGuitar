4f45054ae6445d00b698edbccd6dbabb24e4eaa2
chrisbacon <bacon.chris@gmail.com>
2017-02-28 11:06:01 +0000

    fixed error in add position to exercise migration. should have been integer not string

--- api_server/db/migrate/20170227140401_add_index_to_exercise.rb
+++ api_server/db/migrate/20170227140401_add_index_to_exercise.rb
@@ -1,6 +1,6 @@
 class AddIndexToExercise < ActiveRecord::Migration[5.0]
   def change
-    add_column :exercises, :position, :string
+    add_column :exercises, :position, :integer
     add_index :exercises, [:position, :lesson_id], unique: true
   end
 end
