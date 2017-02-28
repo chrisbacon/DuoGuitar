cad059629ed8a9ad2b8db9f699ac6400b67e2cc5
Elgrogz <gregorgilchrist@gmail.com>
2017-02-28 09:36:20 +0000

    added seperate function for media type

--- desktop_client/client/src/components/exercise.jsx
+++ desktop_client/client/src/components/exercise.jsx
@@ -5,24 +5,28 @@ class Exercise extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {media: null}
   }
 
-  render() {
-    console.log(this.props.item);
-
-    var media = null;
-
+  getMediaType() {
     if (this.props.item.medium.source_type === "video") {
-      media = <iframe src={this.props.item.medium.source} width="500" height="300" frameborder="0" />
-    }   
+      this.state.media = <iframe src={this.props.item.medium.source} width="500" height="300" frameborder="0" />
+    } else if  (this.props.item.medium.source_type === "image") 
+    {
+      this.state.media = <img src={this.props.item.medium.source} width="500" height="300" frameborder="0" />
+    }
+  }
+
+  render() {  
+
+    this.getMediaType();
 
     return (
-        <div className="exercise-container">
-          <h1>{this.props.item.name}</h1>
-          <div className="media-container">{media}</div>
-          <p>{this.props.item.content}</p>
-        </div>
+      <div className="exercise-container">
+      <h1>{this.props.item.name}</h1>
+      <div className="media-container">{this.state.media}</div>
+      <p>{this.props.item.content}</p>
+      </div>
       )
   }
 
