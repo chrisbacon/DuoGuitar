5c985feaabe6b337a2df01aefe83cb2e06b50df2
chrisbacon <bacon.chris@gmail.com>
2017-02-28 10:00:52 +0000

    created backbutton

---
+++ desktop_client/client/src/components/BackButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class BackButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        event.preventDefault()
+        this.props.reset()
+    }
+
+    render() {
+        return <button onClick={this.handleClick}>Back</button>
+    }
+}
