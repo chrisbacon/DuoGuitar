c224b0bbe686bb188f1b01116aa43a4287e7c294
chrisbacon <bacon.chris@gmail.com>
2017-03-01 11:37:51 +0000

    forgot reverse relationship for exercise and lesson

--- api_server/app/models/lesson.rb
+++ api_server/app/models/lesson.rb
@@ -1,6 +1,7 @@
 class Lesson < ApplicationRecord
   belongs_to :course
   has_many :exercises
+  has_many :users, through: :subscribed_lessons, source: :user
   default_scope { order(position: :asc) }
   validates :position, uniqueness: { scope: :course }
 end
