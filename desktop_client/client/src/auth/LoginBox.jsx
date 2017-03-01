1b8d076908759a34d8d066c2b3f47e5f783574cc
chrisbacon <bacon.chris@gmail.com>
2017-03-01 21:04:09 +0000

    fixed merge conflict

--- desktop_client/client/src/auth/LoginBox.jsx
+++ desktop_client/client/src/auth/LoginBox.jsx
@@ -6,10 +6,12 @@ import SignUp from './SignUp'
 class LoginBox extends React.Component {
 
   render () {
-    return (<div>
+    return (<div className='content'>
       <h4> Please Sign In/Up </h4>
-      <SignIn url={this.props.url + "users/sign_in.json"} onSignIn={this.props.setUser}></SignIn>
-      <SignUp url={this.props.url + "users.json"} onSignUp={this.props.setUser}></SignUp>
+      <div className='loginBox'>
+          <SignIn url={this.props.url + "users/sign_in.json"} onSignIn={this.props.setUser}></SignIn>
+          <SignUp url={this.props.url + "users.json"} onSignUp={this.props.setUser}></SignUp>
+      </div>
     </div>)
   }
 }
