c17bfcfd551a621f6392da25614ed48c9ff5d88c
William Robertson <w.robertson305@gmail.com>
2017-03-01 17:35:27 +0000

    tab rendering sort of works

---
+++ desktop_client/client/src/components/tabber.jsx
@@ -0,0 +1,119 @@
+import React from 'react';
+
+class Tabber extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount(){
+    // this.setState({tabString: this.props.tabString})
+    this.checkThings()
+  }
+
+  checkThings(){
+    console.log(this.props.tabString)
+    let tabLinesArray = this.props.tabString.split("break");
+    console.log(tabLinesArray)
+    this.buildTabUnits(tabLinesArray)
+  }
+
+  buildTabUnits(tabLinesArray){
+    var tabColumnsArray = []
+    for(var i = 0; i < tabLinesArray[0].length; i++){
+      var tabColumn = []
+      tabLinesArray.forEach((tabLine) => {
+        tabColumn.push(tabLine[i])
+      })
+      tabColumnsArray.push(tabColumn)
+    }
+    console.log(tabColumnsArray)
+    this.tabColumnsArray = tabColumnsArray;
+  }
+
+  render(){
+    const styles = {
+      tabContainer: {
+        display: "flex"
+      },
+      tabColumn: {
+        margin: "0",
+        padding: "0",
+        border: "1px solid black",
+        fontFamily: "cursive",
+        display: "flex",
+        flexDirection: "column"
+      },
+      tabUnit: {
+        margin: "0",
+        padding: "0",
+        display: "block",
+        width: "20px",
+        height: "20px",
+        border: "1px solid black",
+      },
+      tabUnitText: {
+        margin: "0",
+        padding: "0",
+        display: "block",
+        marginTop: "-50%",
+        width: "100%",
+        height: "100%",
+        border: "1px solid black",
+      },
+      tabUnitBackground: {
+        margin: "0",
+        padding: "0",
+        display: "block",
+        marginTop: "-105%",
+        width: "100%",
+        height: "100%",
+        border: "1px solid black",
+      }
+    }
+
+    if(this.tabColumnsArray){
+
+      let renderedTabArray = []
+
+      this.tabColumnsArray.forEach((tabColumn) => {
+        let renderedTabColumn = []
+        tabColumn.forEach((tabUnit) => {
+          renderedTabColumn.push(<div className="tab-unit" style={styles.tabUnit}><div className="tabUnitText" style={styles.tabUnit}>{(tabUnit!=="-") ? tabUnit : "-"}</div><div style={styles.tabUnitBackground} className="tabUnitBackground"></div></div>)
+        })
+        renderedTabArray.push(renderedTabColumn)
+      })
+
+      console.log(renderedTabArray)
+
+
+
+
+      const renderedTabColumnsArray = renderedTabArray.map((tabUnit) => {
+        return <div style={styles.tabColumn} className="tabColumn">
+          {tabUnit[0]}
+          {tabUnit[1]}
+          {tabUnit[2]}
+          {tabUnit[3]}
+          {tabUnit[4]}
+          {tabUnit[5]}
+        </div>
+      })
+      console.log(renderedTabColumnsArray)
+      // })
+      // renderedTabArray.forEach((renderedTabColumn) => {
+      //   console.log(renderedTabColumn)
+
+      return(
+        <div style={styles.tabContainer} className="tabContainer">
+          {renderedTabColumnsArray}
+        </div>
+      )
+
+    } else {
+      return(null)
+    }
+  }
+}
+
+export default Tabber
--- desktop_client/client/src/containers/Home.jsx
+++ desktop_client/client/src/containers/Home.jsx
@@ -3,6 +3,7 @@ import LoginBox from '../auth/LoginBox'
 import SignOut from '../auth/SignOut'
 import Requester from '../components/requester'
 import DuoGuitar from '../containers/duoguitar'
+import Tabber from '../components/tabber'
 
 class Home extends React.Component {
 
@@ -38,27 +39,28 @@ class Home extends React.Component {
   }
 
   render() {
-    if(this.state.currentUser){
-      return(
-        <div>
-
-        <nav>
-          <div className='navbar'>
-            <h4>DuoGuitar</h4>
-            <div id="user-info">
-              <h4> Welcome {this.state.currentUser.email}</h4>
-              <SignOut url={this.props.url + 'users/sign_out.json'} onSignOut={this.setUser}></SignOut>
-            </div>
-          </div>
-        </nav>
-        <main>
-          <DuoGuitar user={this.state.currentUser} url={this.props.url}/>
-        </main>
-        </div>
-        )
-
-    }
-    return <LoginBox setUser={this.setUser} url={this.props.url} />
+    // if(this.state.currentUser){
+    //   return(
+    //     <div>
+    //
+    //     <nav>
+    //       <div className='navbar'>
+    //         <h4>DuoGuitar</h4>
+    //         <div id="user-info">
+    //           <h4> Welcome {this.state.currentUser.email}</h4>
+    //           <SignOut url={this.props.url + 'users/sign_out.json'} onSignOut={this.setUser}></SignOut>
+    //         </div>
+    //       </div>
+    //     </nav>
+    //     <main>
+    //       <DuoGuitar user={this.state.currentUser} url={this.props.url}/>
+    //     </main>
+    //     </div>
+    //     )
+    //
+    // }
+    // return <LoginBox setUser={this.setUser} url={this.props.url} />
+    return <Tabber tabString="357--break---23break-----break-----break-----break-----break"></Tabber>
   }
 }
 
